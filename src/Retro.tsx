f802984254e9dfae1f113ee7a37bfea0c7e9bcfa
Ian McNally <imcnally@2u.com>
2018-11-04 21:13:09 -0500

    Add saving

--- src/Retro.tsx
+++ src/Retro.tsx
@@ -6,6 +6,11 @@ import border from './styles/border.module.css'
 import backgroundColor from './styles/background-color.module.css'
 import styles from './styles/app.module.css'
 import cx from 'classnames'
+import {
+  addHappyIdeaToRetro,
+  addSadIdeaToRetro,
+  addConfusedIdeaToRetro,
+} from './Database'
 
 function Entry({ children }: { children: string }) {
   const [starred, setStarred] = useState(false)
@@ -90,18 +95,11 @@ function ColumnToggle({
 
 function Compose({
   categories,
+  onSubmit,
 }: {
   categories: { value: string; display: string }[]
+  onSubmit(event: React.FormEvent<any>): void
 }) {
-  const onSubmit = (event: React.FormEvent<any>) => {
-    event.preventDefault()
-    //@ts-ignore
-    const { content, category } = event.target
-    //@ts-ignore
-    console.log('submit', 'content', content.value, 'category', category.value)
-    //@ts-ignore
-    event.target.reset()
-  }
   return (
     <form
       name="compose"
@@ -135,7 +133,7 @@ function Compose({
   )
 }
 
-function Retro() {
+function Retro(props: { retroId: string }) {
   const [showHappyColumn, setShowHappyColumn] = useState(true)
   const [showSadColumn, setShowSadColumn] = useState(true)
   const [showConfusedColumn, setShowConfusedColumn] = useState(true)
@@ -154,56 +152,91 @@ function Retro() {
   const handleStarredColumnToggle = () => {
     setShowStarredColumn(!showStarredColumn)
   }
+  const handleComposeSubmit = (event: React.FormEvent<any>) => {
+    event.preventDefault()
+    const {
+     retroId
+    } = props
+    //@ts-ignore
+    const { content, category } = event.target
+    //@ts-ignore
+    console.log('submit', 'content', content.value, 'category', category.value)
+    switch (category.value) {
+      case 'happy':
+        addHappyIdeaToRetro(
+          { content: content.value, starred: false, votes: 0 },
+          retroId,
+        )
+        break
+      case 'sad':
+        addSadIdeaToRetro(
+          { content: content.value, starred: false, votes: 0 },
+          retroId,
+        )
+        break
+      case 'confused':
+        addConfusedIdeaToRetro(
+          { content: content.value, starred: false, votes: 0 },
+          retroId,
+        )
+        break
+      default:
+        break
+    }
+    //@ts-ignore
+    event.target.reset()
+  }
   return (
     <>
       <h1 className={visually.hidden}>Retro</h1>
       <main className={styles.main}>
         <Columns>
-          <Column title="Happy" show={showHappyColumn}>
+          <Column title="happy" show={showHappyColumn}>
             <Entry>This is good</Entry>
             <Entry>This is good, too</Entry>
           </Column>
-          <Column title="Sad" show={showSadColumn}>
+          <Column title="sad" show={showSadColumn}>
             <Entry>This is sad</Entry>
             <Entry>This is sad, too</Entry>
           </Column>
-          <Column title="Confused" show={showConfusedColumn}>
+          <Column title="confused" show={showConfusedColumn}>
             <Entry>This is confused</Entry>
             <Entry>This is confused, too</Entry>
           </Column>
-          <Column title="Starred" show={showStarredColumn} />
+          <Column title="starred" show={showStarredColumn} />
         </Columns>
         <ColumnToggles>
           <ColumnToggle
             emoji={'👍'}
-            toggleFor="Happy"
+            toggleFor="happy"
             onClick={handleHappyColumnToggle}
             on={showHappyColumn}
           />
           <ColumnToggle
             emoji={'👎'}
-            toggleFor="Sad"
+            toggleFor="sad"
             onClick={handleSadColumnToggle}
             on={showSadColumn}
           />
           <ColumnToggle
             emoji={'😕'}
-            toggleFor="Confused"
+            toggleFor="confused"
             onClick={handleConfusedColumnToggle}
             on={showConfusedColumn}
           />
           <ColumnToggle
             emoji={'⭐️'}
-            toggleFor="Starred"
+            toggleFor="starred"
             onClick={handleStarredColumnToggle}
             on={showStarredColumn}
           />
         </ColumnToggles>
         <Compose
+          onSubmit={handleComposeSubmit}
           categories={[
-            { value: 'Happy', display: '👍' },
-            { value: 'Sad', display: '👎' },
-            { value: 'Confused', display: '😕' },
+            { value: 'happy', display: '👍' },
+            { value: 'sad', display: '👎' },
+            { value: 'confused', display: '😕' },
           ]}
         />
       </main>
